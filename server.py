97958c20cc4c9c3d8e417f5ec41942616527a82e
wdzbjz548q-netizen <wdzbjz548q@privaterelay.appleid.com>
2026-01-31 01:34:30 +0000

    Update server.py

--- server.py
+++ server.py
@@ -18,8 +18,9 @@ import httpx
 ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
+import certifi
 mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(mongo_url)
+client = AsyncIOMotorClient(mongo_url, tlsCAFile=certifi.where())
 db = client[os.environ['DB_NAME']]
 
 app = FastAPI()
